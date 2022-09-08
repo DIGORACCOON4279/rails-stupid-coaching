40ab3b2cc012ed7a9a27419d84d004be5a215819
diego marin <dilema4000@gmail.com>
2022-09-07 21:24:12 -0500

    Challenge Stupid Coach2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  #root to: "questions#ask" # es como un home, la primera ruta
+  # root to: "questions#ask" # es como un home, la primera ruta
   # Defines the root path route ("/")
   # root "articles#index"
   get "/ask", to: "questions#ask"
   get "/answer", to: "questions#answer"
+  get "/ask", to: "questions#ask"
 
 end
