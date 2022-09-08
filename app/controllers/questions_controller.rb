3031d37f30568faecdc62e2450c78645d8d30a41
diego marin <dilema4000@gmail.com>
2022-09-07 19:26:52 -0500

    Challenge Stupid Coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,16 @@
 class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @ask = params[:ask]
+    if @ask == "I´m going to work"
+      @answer = "Great"
+    elsif @ask == "Can I go shopping?"
+      @answer = "Silly question, get dressed and go to work!."
+    else @ask == "I come down with the flu"
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
